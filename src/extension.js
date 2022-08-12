25d435f9fd4d88ac70c04d9489168515c07c821f
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-08-12 11:48:49 -0300

    Javascript code to show snippets when hovering mouse.

---
+++ src/extension.js
@@ -0,0 +1,28 @@
+const vscode = require('vscode');
+
+const ISLASH_SNIPPETS = require('../snippets/islash-code-snippets.json')
+
+function activate() {
+    console.log("ISlash extension activated.");
+    vscode.languages.registerHoverProvider('islash', {
+        provideHover(document, position) {
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+            const snippet = ISLASH_SNIPPETS[word]
+            if (snippet) {
+                const content = new vscode.MarkdownString(snippet.description)
+                content.appendCodeblock(snippet.body[0])
+                return new vscode.Hover(content, new vscode.Range(position, position));
+            }
+        }
+    });
+}
+
+function deactivate() {
+    console.log("ISlash extension deactivated.")
+}
+
+module.exports = {
+    activate,
+    deactivate
+}
